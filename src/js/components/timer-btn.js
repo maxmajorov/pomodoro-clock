60454eaf8c52a8236240d5d4150344bd403bae57
Maxim Predko <maxpredko@gmail.com>
2022-01-06 18:42:39 +0300

    feat: done functions for timers

--- src/js/components/timer-btn.js
+++ src/js/components/timer-btn.js
@@ -4,61 +4,149 @@ const time = document.querySelector('.time')
 const startTimer = document.querySelector('.start')
 const resetTimer = document.querySelector('.clear')
 
-let inputMinutes = prompt('input minutes') * 60 //потом буду брать из settings
+let inputMinutes = 0.5 * 60 //потом буду брать из settings
+let shortIntMin = 0.2 * 60 // минуты для короткого интервала
+let longIntMin = 5 * 60 // минуты для long интервала
 
 
 
-timerBtns[0].onclick = (e) => {
-    e.preventDefault()
-    body.style.backgroundColor = 'rgb(189, 74, 74)' 
-    body.style.transition = 'background-color 0.5s ease-in-out 0s' 
+let mainTimerClick = timerBtns[0].onclick = (e) => {
+    // e.preventDefault()
+    body.style.backgroundColor = 'rgb(189, 74, 74)'     
 }
 
-timerBtns[1].onclick = (e) => {
-    e.preventDefault()
+let shortTimerClick = timerBtns[1].onclick = (e) => {
+    // e.preventDefault()
     body.style.backgroundColor = 'rgb(76, 145, 149)'
-    time.textContent = '05:00'
+    body.style.transition = 'background-color 0.5s ease-in-out 0s'  
 
+    startTimer.onclick = () => {
+        getIntervalID()    // вызывая функцию запускается таймер присваевается значение intervalID    
+    
+        resetTimer.onclick = () => {
+            clearInterval(IntervalID)
+            time.textContent = '00:00'
+        }
+    }
     
 }
 
 timerBtns[2].onclick = (e) => {
     e.preventDefault()
     body.style.backgroundColor = 'rgb(74, 79, 189)'
-    time.textContent = '25:00'
+    
+    startTimer.onclick = () => {
+        getLongIntervalID()    // вызывая функцию запускается таймер присваевается значение intervalID    
+    
+        resetTimer.onclick = () => {
+            clearInterval(longIntervalID)
+            time.textContent = '00:00'
+        }
+    }
 }
 
-let interval = setInterval(() => {
+// ------INTERVALS------
+
+let intervalID
+let longIntervalID
+
+function getIntervalID() {
+    intervalID = setInterval(pomodoroInterval, 1000)  
+}
+
+function getLongIntervalID() {
+    longIntervalID = setInterval(longInterval, 1000)  
+}
+
+// ------POMODORO TIMER-----
+
+function pomodoroInterval() {
     let sec = Math.floor(inputMinutes % 60)  //Получаем секунды
     let min = Math.floor(inputMinutes / 60 % 60)   // Получаем минуты
     let hour = Math.floor(inputMinutes / 60 / 60 % 60)  // Получаем часы
     let strTimer = ''
-
+    
     if (inputMinutes > 0 && inputMinutes <= 3600) {
-        strTimer = `${Math.trunc(min)}:${sec}`
+        strTimer = `${(min+'').padStart(2, 0)}:${(sec+'').padStart(2, 0)}`
         time.textContent = strTimer
         --inputMinutes  // Уменьшаем таймер
     } else {
-        strTimer = `${Math.trunc(hour)}:${Math.trunc(min)}:${sec}`
+        strTimer = `${(hour+'').padStart(2, 0)}:${(min+'').padStart(2, 0)}:${(sec+'').padStart(2, 0)}`
         time.textContent = strTimer
         --inputMinutes 
     }
 
-    if (inputMinutes < 0 || ) {
-        clearInterval(interval)
-        time.textContent = '00:00'
+    if (inputMinutes == 0) {
+        clearInterval(intervalID)
+        shortTimerClick()
+        setInterval(shortInterval, 1000)
     } 
-}, 1000)
+}
 
+// ----FOR SHORT BRAEK----
+
+function shortInterval() {
+    let sec = Math.floor(shortIntMin % 60)  //Получаем секунды
+    let min = Math.floor(shortIntMin / 60 % 60)   // Получаем минуты
+    let strTimer = ''
+
+    if (shortIntMin > 0) {
+        strTimer = `${(min+'').padStart(2, 0)}:${(sec+'').padStart(2, 0)}`
+        time.textContent = strTimer
+        --shortIntMin  // Уменьшаем таймер
+        }
+}
+
+// -----LONG BREAK----
+
+function longInterval() {
+    let sec = Math.floor(longIntMin % 60)  //Получаем секунды
+    let min = Math.floor(longIntMin / 60 % 60)   // Получаем минуты
+    let strTimer = ''
+
+    if (longIntMin > 0 && longIntMin <= 3600) {
+        strTimer = `${(min+'').padStart(2, 0)}:${(sec+'').padStart(2, 0)}`
+        time.textContent = strTimer
+        --longIntMin  // Уменьшаем таймер
+    } else {
+        strTimer = `${(hour+'').padStart(2, 0)}:${(min+'').padStart(2, 0)}:${(sec+'').padStart(2, 0)}`
+        time.textContent = strTimer
+        --longIntMin
+    }
+}
+
+let playBtn = document.querySelector('.playMusic')
+
+let audio = new Audio('../../audio/Enigma.mp3')
+console.log(audio)
+// audio.play()
+
+playBtn.onclick = () => { audio.play() }
 
 startTimer.onclick = () => {
-    interval()    
+    getIntervalID() // вызывая функцию запускается таймер присваевается значение intervalID    
+
+    
+    // const repeatPromise = new Promise((res, rej) => {
+    //     setInterval(() => {
+    //        res(getIntervalID()) 
+    //     }, ((inputMinutes + shortIntMin) * 1000));
+    // })
+
+    // repeatPromise.then((val) => val) 
+
+    // начинает срабатывать на второй круг но идет не от заданного занчения???
+
+   
+    
 }
 
 resetTimer.onclick = () => {
-    console.log(interval)
-    clearInterval()
+    clearInterval(intervalID) // Продумать!!!
+    time.textContent = '00:00'
 }
+
+
 
 
 
